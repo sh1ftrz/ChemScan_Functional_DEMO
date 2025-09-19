56267bec8c4541d88caa00fb18d592180377486b
sh1ftrz <rockythanapat@gmail.com>
2025-09-19 18:59:12 +0700

    Update 0.1

--- app/src/main/java/com/example/chemapp30/CalculationActivity.java
+++ app/src/main/java/com/example/chemapp30/CalculationActivity.java
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CalculationActivity extends AppCompatActivity {
 
     private EditText edtInput1, edtInput2;
-    private Button btnWV, btnVV;
+    private Button btnMol, btnWW, btnWV, btnVV;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,13 +27,48 @@ public class CalculationActivity extends AppCompatActivity {
 
         edtInput1 = findViewById(R.id.edtInput1);
         edtInput2 = findViewById(R.id.edtInput2);
+
+        btnMol = findViewById(R.id.btnMol);
+
+        btnWW = findViewById(R.id.btnWW);
         btnWV = findViewById(R.id.btnWV);
         btnVV = findViewById(R.id.btnVV);
 
+        btnMol.setOnClickListener(v -> calculateMol());
+
+        btnWW.setOnClickListener(v -> calculateWW());
         btnWV.setOnClickListener(v -> calculateWV());
         btnVV.setOnClickListener(v -> calculateVV());
     }
 
+    private void calculateMol() {
+        try {
+            double solute = parseDouble(edtInput1);
+            double volume = parseDouble(edtInput2);
+            if (volume <= 0) {
+                Toast.makeText(this, "ปริมาตรต้องมากกว่า 0", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            double wv = solute / volume;
+            Toast.makeText(this, String.format("Mol = %.2f Mol", wv), Toast.LENGTH_LONG).show();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "กรุณากรอกตัวเลขให้ถูกต้อง", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void calculateWW() {
+        try {
+            double solute = parseDouble(edtInput1);
+            double volume = parseDouble(edtInput2);
+            if (volume <= 0) {
+                Toast.makeText(this, "ปริมาตรต้องมากกว่า 0", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            double wv = solute / volume * 100.0;
+            Toast.makeText(this, String.format("w/w = %.2f%%", wv), Toast.LENGTH_LONG).show();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "กรุณากรอกตัวเลขให้ถูกต้อง", Toast.LENGTH_SHORT).show();
+        }
+    }
     private void calculateWV() {
         try {
             double solute = parseDouble(edtInput1);
